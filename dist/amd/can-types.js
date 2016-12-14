ed4c6f6245ef9e46b5815580e7fcfe887bdd07f8
Kevin Phillips <kphillips86@gmail.com>
2016-12-14 10:29:33 -0600

    0.1.0

---
+++ dist/amd/can-types.js
@@ -0,0 +1,49 @@
+/*can-types@0.0.1#can-types*/
+define(function (require, exports, module) {
+    var namespace = require('can-namespace');
+    var isPromise = require('can-util/js/is-promise');
+    var types = {
+        isMapLike: function () {
+            return false;
+        },
+        isListLike: function () {
+            return false;
+        },
+        isPromise: function (obj) {
+            return isPromise(obj);
+        },
+        isConstructor: function (func) {
+            if (typeof func !== 'function') {
+                return false;
+            }
+            for (var prop in func.prototype) {
+                return true;
+            }
+            return false;
+        },
+        isCallableForValue: function (obj) {
+            return typeof obj === 'function' && !types.isConstructor(obj);
+        },
+        isCompute: function (obj) {
+            return obj && obj.isComputed;
+        },
+        iterator: typeof Symbol === 'function' && Symbol.iterator || '@@iterator',
+        DefaultMap: null,
+        DefaultList: null,
+        queueTask: function (task) {
+            var args = task[2] || [];
+            task[0].apply(task[1], args);
+        },
+        wrapElement: function (element) {
+            return element;
+        },
+        unwrapElement: function (element) {
+            return element;
+        }
+    };
+    if (namespace.types) {
+        throw new Error('You can\'t have two versions of can-types, check your dependencies');
+    } else {
+        module.exports = namespace.types = types;
+    }
+});
